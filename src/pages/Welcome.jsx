cb24a9d861392bc80c180d47aa67504e23964824
Selva Ganapathi <60595640+selva1011@users.noreply.github.com>
2024-03-03 12:57:53 +0530

    duration removed

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -6,6 +6,9 @@ const Welcome = () => {
       <div className="h-screen flex items-center justify-center">
         <div className="border-2 border-gray-300 rounded-2xl p-5 w-96">
           <div className="text-2xl font-bold text-gray-700">Welcome</div>
+          <a href="/register">
+            <button className="hover:bg-green-400 bg-green-500  text-white text-sm font-bold rounded-2xl w-full py-3 mt-7 border-b-4 border-b-green-600 text-center hover:cursor-pointer">NNNNN</button>
+          </a>
           <div className="hover:bg-green-400 bg-green-500  text-white text-sm font-bold rounded-2xl w-full py-3 mt-7 border-b-4 border-b-green-600 text-center hover:cursor-pointer">
             <Link to="/register">CREATE A PROFILE</Link>
           </div>
